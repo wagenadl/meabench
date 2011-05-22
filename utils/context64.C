dce1c71bc7ecb3c5cb3e1fc91f9581aa8d28cca5
Daniel A. Wagenaar <dawatsigncaltechperiodedu>
2011-05-21 22:27:21 -0700

    New common header "MultiFile.H" to easily read from "-1", etc., continuation files. Not yet actually tested for real multifile data sets, but it works for single files. StreamClient now uses this.

--- utils/context64.C
+++ utils/context64.C
@@ -38,115 +38,130 @@ enum {
 };
 
 int main(int argc, char **argv) {
-  Argh arg;
-  arg.flag(argHELP, "-h --help This help");
-  arg.arg(argOUTPUT, "-o Outputfile Where output goes. Default is stdout.");
-  arg.arg(argSPKIN, "-i Spikefile Where spikes come from. Default is stdin.");
-  arg.arg(argPRECTXT, "-p25 --pre Prectxt Number of samples before peak.");
-  arg.arg(argPOSTCTXT, "-P49 --post Postctxt Number of samples after peak.");
-  arg.flag(argTEXT,"-t Input is text file: Read spike time stamps and channel selection from input lines.");
-  arg.flag(argCR,"-8 --cr Channel numbers are MCS 8x8 rather than hardware");
+  try {
+    Argh arg;
+    arg.flag(argHELP, "-h --help This help");
+    arg.arg(argOUTPUT, "-o Outputfile Where output goes. Default is stdout.");
+    arg.arg(argSPKIN, "-i Spikefile Where spikes come from. Default is stdin.");
+    arg.arg(argPRECTXT, "-p25 --pre Prectxt Number of samples before peak.");
+    arg.arg(argPOSTCTXT, "-P49 --post Postctxt Number of samples after peak.");
+    arg.flag(argTEXT,"-t Input is text file: Read spike time stamps and channel selection from input lines.");
+    arg.flag(argCR,"-8 --cr Channel numbers are MCS 8x8 rather than hardware");
 
-  int arg0 = arg.parse(argc, argv);
-  if (arg0<0)
-    return 1;
-  if (arg.has(argHELP)) {
-    arg.help("context64: Extract 64 channels worth of context from spike files", "Spike files must be in correct time order. (Pipe through 'spikeorder' if\nin doubt.)");
-    return 1;
-  }
-  if (arg0!=argc-1) {
-    arg.usage("context64", "rawstream|rawfile");
-    return 1;
-  }
-
-  bool istext = arg.has(argTEXT);
-  bool isCR = arg.has(argCR);
-
-  FILE *out = stdout;
-  FILE *spkin = stdin;
-
-  if (arg.has(argOUTPUT)) {
-    out = fopen(arg.get(argOUTPUT).c_str(), "wb");
-    if (!out) {
-      perror("Cannot write output");
-      return 2;
+    int arg0 = arg.parse(argc, argv);
+    if (arg0<0)
+      return 1;
+    if (arg.has(argHELP)) {
+      arg.help("context64: Extract 64 channels worth of context from spike files",
+	       "When reading from a raw stream, spike files must be in correct time order.\n(Pipe through 'spikeorder' if in doubt.) When reading from a file,\nthis is not required.\nIf spikes are requested too near beginning or end of raw file, their\ncontext will contain garbage.");
+      return 1;
     }
-  }
-  if (arg.has(argSPKIN)) {
-    spkin = fopen(arg.get(argSPKIN).c_str(), istext ? "r" : "rb");
-    if (!spkin) {
-      perror("Cannot read spikes");
-      return 2;
+    if (arg0!=argc-1) {
+      arg.usage("context64", "rawstream|rawfile");
+      return 1;
     }
-  }
 
-  std::string rawname = argv[arg0];
-  StreamRaw *rawstream = 0;
-  if (rawname.find_first_of(".")==rawname.npos) 
-    rawstream = new StreamRaw(rawname, true); // open stream
-  else
-    rawstream = new StreamRaw(rawname); // open file
+    bool istext = arg.has(argTEXT);
+    bool isCR = arg.has(argCR);
+
+    FILE *out = stdout;
+    FILE *spkin = stdin;
+
+    if (arg.has(argOUTPUT)) {
+      out = fopen(arg.get(argOUTPUT).c_str(), "wb");
+      if (!out) {
+	perror("Cannot write output");
+	return 2;
+      }
+    }
+    if (arg.has(argSPKIN)) {
+      spkin = fopen(arg.get(argSPKIN).c_str(), istext ? "r" : "rb");
+      if (!spkin) {
+	perror("Cannot read spikes");
+	return 2;
+      }
+    }
+
+    std::string rawname = argv[arg0];
+    StreamRaw *rawstream = 0;
+    if (rawname.find_first_of(".")==rawname.npos) 
+      rawstream = new StreamRaw(rawname, true); // open stream
+    else
+      rawstream = new StreamRaw(rawname); // open file
   
-  int prectxt = atoi(arg.get(argPRECTXT).c_str());
-  int postctxt = atoi(arg.get(argPOSTCTXT).c_str());
-  int ctxt = prectxt + postctxt;
-  Sample buffer[ctxt];
-  int bufidx=0;
-  Spikeinfo spk;
-  timeref_t rawtime = ctxt;
-  if (!rawstream->read(buffer, ctxt))
-    return 2;
+    int prectxt = atoi(arg.get(argPRECTXT).c_str());
+    int postctxt = atoi(arg.get(argPOSTCTXT).c_str());
+    int ctxt = prectxt + postctxt;
+    Sample buffer[ctxt];
+    int bufidx=0;
+    Spikeinfo spk;
+    timeref_t rawtime = ctxt;
+    if (!rawstream->read(buffer, ctxt))
+      return 2;
 
 
-  if (istext) {
-    raw_t obuf[64*ctxt];
-    char line[1024];
-    int hww[64];
-    while (fgets(line, 1024, spkin)) {
-      char *timeptr = strtok(line, " ");
-      timeref_t nexttime = atoll(timeptr);
-      while (nexttime+postctxt > rawtime) { // skip to next spike
-	if (!rawstream->read(buffer+bufidx, 1))
-	  return 2;
-	rawtime++;
-	if (++bufidx>=ctxt)
-	  bufidx=0;
+    if (istext) {
+      raw_t obuf[64*ctxt];
+      char line[1024];
+      int hww[64];
+      while (fgets(line, 1024, spkin)) {
+	char *timeptr = strtok(line, " ");
+	timeref_t nexttime = atoll(timeptr);
+	if (nexttime+postctxt<rawtime) {
+	  if (nexttime<prectxt) {
+	    rawtime = 0;
+	    fprintf(stderr, "Warning: cannot seek before start of file\n");
+	  } else {
+	    rawtime = nexttime - prectxt;
+	  }
+	  rawstream->seek(rawtime*sizeof(Sample));
+	}
+	while (nexttime+postctxt > rawtime) { // skip to next spike
+	  if (!rawstream->read(buffer+bufidx, 1))
+	    return 2;
+	  rawtime++;
+	  if (++bufidx>=ctxt)
+	    bufidx=0;
+	}
+	char *chptr;
+	int n=0;
+	while ((chptr = strtok(0, " "))) 
+	  hww[n++] = atoi(chptr);
+	if (isCR)
+	  for (int c=0; c<n; c++)
+	    hww[c] = cr12hw(hww[c]);
+	//fprintf(stderr,"%Li", rawtime);
+	//for (int c=0; c<n; c++)
+	//	fprintf(stderr," %i",hww[c]);
+	//fprintf(stderr,"\n");
+	raw_t *optr=obuf;
+	for (int t=0; t<ctxt; t++) 
+	  for (int c=0; c<n; c++)
+	    *optr++ = buffer[(t+bufidx)%ctxt][hww[c]];
+	fwrite(obuf, sizeof(raw_t)*n, prectxt+postctxt, out);
+      }      
+    } else {
+      Sample obuf[ctxt];
+      while (fread(&spk, SpikeinfoDISKSIZE, 1, spkin)==1) {
+	timeref_t nexttime = spk.time;
+	//fprintf(stderr, "Skipping to %.3f s\n", nexttime/25e3);
+	while (nexttime+postctxt > rawtime) { // skip to next spike
+	  if (!rawstream->read(buffer, 1))
+	    return 2;
+	  rawtime++;
+	}
+	Sample *optr=obuf;
+	for (int t=0; t<ctxt; t++) {
+	  Sample *iptr = buffer+(t+bufidx)%ctxt;
+	  *optr++ = *iptr;
+	}
+	fwrite(obuf, sizeof(Sample), ctxt, out);
       }
-      char *chptr;
-      int n=0;
-      while ((chptr = strtok(0, " "))) 
-	hww[n++] = atoi(chptr);
-      if (isCR)
-	for (int c=0; c<n; c++)
-	  hww[c] = cr12hw(hww[c]);
-      fprintf(stderr,"%Li", rawtime);
-      for (int c=0; c<n; c++)
-	fprintf(stderr," %i",hww[c]);
-      fprintf(stderr,"\n");
-      raw_t *optr=obuf;
-      for (int t=0; t<ctxt; t++) 
-	for (int c=0; c<n; c++)
-	  *optr++ = buffer[(t+bufidx)%ctxt][hww[c]];
-      fwrite(obuf, sizeof(raw_t)*n, prectxt+postctxt, out);
-    }      
-  } else {
-    Sample obuf[ctxt];
-    while (fread(&spk, SpikeinfoDISKSIZE, 1, spkin)==1) {
-      timeref_t nexttime = spk.time;
-      fprintf(stderr, "Skipping to %.3f s\n", nexttime/25e3);
-      while (nexttime+postctxt > rawtime) { // skip to next spike
-	if (!rawstream->read(buffer, 1))
-	  return 2;
-	rawtime++;
-      }
-      Sample *optr=obuf;
-      for (int t=0; t<ctxt; t++) {
-	Sample *iptr = buffer+(t+bufidx)%ctxt;
-	*optr++ = *iptr;
-      }
-      fwrite(obuf, sizeof(Sample), ctxt, out);
     }
+    return 0;
+  } catch (Error const &e) {
+    e.report();
+    return 2;
   }
-  return 0;
 }
  
